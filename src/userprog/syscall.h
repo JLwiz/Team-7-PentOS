6ec3ecefbcc31e335ffcb27f45939651ed8b88dc
andrew-k-fryer <fryerak@dukes.jmu.edu>
2022-10-15 14:36:16 -0400

    Forgot to include .h file

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,10 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+void exit (int status);
+int read (int fd, void *buffer, unsigned length);
+int write (int fd, const void *buffer, unsigned length);
 void syscall_init (void);
+
 
 #endif /* userprog/syscall.h */
