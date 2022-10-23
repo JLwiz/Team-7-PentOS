26b118ea3154c6b75da4e0631d0bdd19b977db75
Patrick Glebus <glebuspm@dukes.jmu.edu>
2022-10-23 14:18:56 -0400

    added basic semaphores

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -91,7 +91,7 @@ struct thread
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
 
-    struct semaphore testing_sema;
+    
     
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
