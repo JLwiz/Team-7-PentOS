256f53c78fec043538ade85b20010b74f3529cfe
JLwiz <lewisjw@dukes.jmu.edu>
2022-11-08 19:16:11 -0500

    file_descriptor changed to hash_elem.

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -92,7 +92,7 @@ struct thread
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
     
-    struct list_elem file_descriptors;  /* List element for filedescriptor list*/
+    struct hash_elem file_descriptors;  /* List element for filedescriptor list*/
     struct hash fd_hash;
     
     /* Shared between thread.c and synch.c. */
