2ea014e3fbb9638fab0c2a9ebf101f8c298ce5f6
JLwiz <lewisjw@dukes.jmu.edu>
2022-12-07 21:33:32 -0500

    less changes.

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -48,7 +48,7 @@ bool list_less_func_sort_by_priority_synch(const struct list_elem *a,
 {
   struct thread *a_prio = list_entry(a, struct thread, elem);
   struct thread *b_prio = list_entry(b, struct thread, elem);
-  return a_prio->priority >= b_prio->priority;
+  return a_prio->priority > b_prio->priority;
 }
 
 /* Initializes semaphore SEMA to VALUE.  A semaphore is a
